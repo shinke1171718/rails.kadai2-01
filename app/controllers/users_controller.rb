1523be4165f72ec77801e397f1cf3171bd55254c
Apple <apple@ShinkenoMacBook.local>
2022-11-07 19:15:14 +0900

    ログイン画面完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,33 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
-
 end
